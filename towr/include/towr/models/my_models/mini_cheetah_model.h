45d3c377de11238321434d99f66f2b15cb68ea0f
Nolan <nfey@nd.edu>
2020-10-13 20:28:18 -0400

    First commit

--- towr/include/towr/models/my_models/mini_cheetah_model.h
+++ towr/include/towr/models/my_models/mini_cheetah_model.h
@@ -27,44 +27,48 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ******************************************************************************/
 
-#include <towr/models/robot_model.h>
+#ifndef TOWR_TOWR_ROS_INCLUDE_TOWR_ROS_MINI_CHEETAH_MODEL_H_
+#define TOWR_TOWR_ROS_INCLUDE_TOWR_ROS_MINI_CHEETAH_MODEL_H_
 
-#include <towr/models/examples/monoped_model.h>
-#include <towr/models/examples/biped_model.h>
-#include <towr/models/examples/hyq_model.h>
-#include <towr/models/examples/anymal_model.h>
-#include <towr/models/my_models/mini_cheetah_model.h>
+#include <towr/models/kinematic_model.h>
+#include <towr/models/single_rigid_body_dynamics.h>
+#include <towr/models/endeffector_mappings.h>
 
 namespace towr {
 
+/**
+ * @brief The Kinematics of the quadruped robot Mini Cheetah.
+ */
+class MiniCheetahKinematicModel : public KinematicModel {
+public:
+  MiniCheetahKinematicModel () : KinematicModel(4)
+  {
+    const double x_nominal_b = 0.34;
+    const double y_nominal_b = 0.19;
+    const double z_nominal_b = -0.42;
 
-RobotModel::RobotModel(Robot robot)
-{
-  switch (robot) {
-    case Monoped:
-      dynamic_model_   = std::make_shared<MonopedDynamicModel>();
-      kinematic_model_ = std::make_shared<MonopedKinematicModel>();
-      break;
-    case Biped:
-      dynamic_model_   = std::make_shared<BipedDynamicModel>();
-      kinematic_model_ = std::make_shared<BipedKinematicModel>();
-      break;
-    case Hyq:
-      dynamic_model_   = std::make_shared<HyqDynamicModel>();
-      kinematic_model_ = std::make_shared<HyqKinematicModel>();
-      break;
-    case Anymal:
-      dynamic_model_   = std::make_shared<AnymalDynamicModel>();
-      kinematic_model_ = std::make_shared<AnymalKinematicModel>();
-    case MiniCheetah:
-      dynamic_model_ = std::make_shared<MiniCheetahDynamicModel>();
-      kinematic_model_= std::make_shared<MiniCheetahKinematicModel>();
-      break;
-    default:
-      assert(false); // Error: Robot model not implemented.
-      break;
+    nominal_stance_.at(LF) <<  x_nominal_b,   y_nominal_b, z_nominal_b;
+    nominal_stance_.at(RF) <<  x_nominal_b,  -y_nominal_b, z_nominal_b;
+    nominal_stance_.at(LH) << -x_nominal_b,   y_nominal_b, z_nominal_b;
+    nominal_stance_.at(RH) << -x_nominal_b,  -y_nominal_b, z_nominal_b;
+
+    max_dev_from_nominal_ << 0.15, 0.1, 0.10;
   }
-}
+};
 
+/**
+ * @brief The Dynamics of the quadruped robot Mini Cheetah.
+ source: https://github.com/mit-biomimetics/Cheetah-Software/blob/master/common/
+ include/Dynamics/MiniCheetah.h
+ */
+class MiniCheetahDynamicModel : public SingleRigidBodyDynamics {
+public:
+  MiniCheetahDynamicModel()
+  : SingleRigidBodyDynamics(3.3,
+                    11253, 0, 0, 36203, 0, 42673,
+                    4) {}
+};
 
 } // namespace towr
+
+#endif /* TOWR_TOWR_ROS_INCLUDE_TOWR_ROS_MINI_CHEETAH_MODEL_H_ */
